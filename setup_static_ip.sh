a75bb6b202b377d18c7bc7c3ef781426e90f28fc
Eloi Riera <hoquei0021@gmail.com>
2023-02-05 15:59:51 +0100

    Setting up upgrade fixed_ip file

--- setup_static_ip.sh
+++ setup_static_ip.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+
+curl -LJO https://raw.githubusercontent.com/junkers21/raspberry_ubuntu_setup/main/static_ip.sh
+sudo chmod +x static_ip.sh
+
 if test -f "/usr/local/bin/static_ip"; then
     echo "Ip fixing cron already setted"
+    echo "Upgrading file"
+
+    sudo mv -f static_ip.sh /usr/local/bin/static_ip
 else
     echo "Setting up Ip fixing cron on reboot"
-
-    curl -LJO https://raw.githubusercontent.com/junkers21/raspberry_ubuntu_setup/main/static_ip.sh
-    sudo chmod +x static_ip.sh
+    
     sudo mv static_ip.sh /usr/local/bin/static_ip
 
     echo "Adding file to reboot cron"
     (crontab -l ; echo "@reboot bash /usr/local/bin/static_ip") | crontab -
-
-    echo "Executing for the first time"
-    sudo bash /usr/local/bin/static_ip
-    echo "Ip fixed"
 fi
+
+echo "Fixing IP"
+sudo bash /usr/local/bin/static_ip
+echo "Ip fixed"
